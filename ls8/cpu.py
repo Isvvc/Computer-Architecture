9573b21959dc518d812e1c979a034ff4b07cb12e
Isvvc <lyons@tuta.io>
2020-04-20 14:38:44 -0600

    Add inventory and RAM functions

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,9 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 8
+        self.reg = [0] * 8
+        self.pc = 0
 
     def load(self):
         """Load a program into memory."""
@@ -59,6 +61,12 @@ class CPU:
             print(" %02X" % self.reg[i], end='')
 
         print()
+
+    def ram_read(mar):
+        return self.ram[mar]
+
+    def ram_write(mar, data):
+        self.ram[mar] = data
 
     def run(self):
         """Run the CPU."""
